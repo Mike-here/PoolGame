aeede2012903431399ff8b787bbc7aa353e3bda3
Michael <macheampong287@gmail.com>
2024-10-17 08:09:44 -0400

    Created and displayed ball on the screen

--- mainpy/pool_game.py
+++ mainpy/pool_game.py
@@ -1,13 +1,34 @@
 import pygame
 import sys
+import pymunk
+import pymunk.pygame_util
 
 pygame.init()
 
+# Screen sizes.
 screen_width = 1200
 screen_height = 630
 
+# Game window
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("The Real Pool Game")
+
+# Pymunk space.
+space = pymunk.Space()
+draw_options  = pymunk.pygame_util.DrawOptions(screen)
+
+# Create a ball.
+def create_ball(radius, pos):
+    body = pymunk.Body()
+    body.position = pos
+    shape = pymunk.Circle(body, radius)
+    shape.mass = 3
+
+    space.add(body, shape)
+    return shape
+
+
+created_ball = create_ball(25, (800, 400))
 
 
 # Game loop.
@@ -18,5 +39,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    space.debug_draw(draw_options)
+    pygame.display.update()        
             
 pygame.quit()
